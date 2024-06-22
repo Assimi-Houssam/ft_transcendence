c6fce27bc2dc13654d582169a5e391a5fb8a7192
AMAZZAL-AMINE <amazzalhusa@gmail.com>
2024-06-22 16:52:22 +0100

    finished the components style

--- src/components/LayoutComponent.js
+++ src/components/LayoutComponent.js
@@ -1,14 +1,33 @@
 import { router } from "../routes/routes.js";
-
-let sidebar_links = ["/"]; // add the name of the sidebar routes paths here 
+let sidebar_links = ["/"]; // add the name of the sidebar routes paths here
 
 const Navbar = () => {
   let navbar = document.createElement("nav");
   navbar.classList.add("navbar_");
+  let log_username = "mamazzal1337425555555";
   navbar.innerHTML = `
-        <div class="nav_search_">
-            <div> <p> icon </p> </div>
-            <div> <input type="search" /> </div>
+        <div class="nav_search_ gradient-dark-bg gradient-dark-border">
+            <img src="../../assets/icons/search.png" />
+            <input
+              type="text" 
+              placeholder="Search for user by email or username"
+            />
+        </div>
+        <div class="navbar_right_elements" >
+            <div class="navbar_notification_btn gradient-dark-bg gradient-dark-border" >
+              <div class="dot_"></div>
+              <img src="../../assets/icons/notification.png" />
+            </div>
+            <div class="nav_username gradient-dark-bg gradient-dark-border">
+              <p class="nav_username_id">
+              ${
+                log_username.length > 10
+                  ? log_username.slice(0, 13) + "..."
+                  : log_username
+              }
+              </p>
+              <img src="./assets/images/mamazzal.jpg" />
+            </div>
         </div>
     `;
   return navbar;
@@ -45,7 +64,6 @@ const Sidebar = () => {
     `;
   return sidebar;
 };
-
 
 export class LayoutWrraper extends HTMLElement {
   children;
