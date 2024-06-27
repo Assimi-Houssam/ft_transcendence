921c873e75b677754d0fae8b38d0529a228da821
silentzer <hamidkorchi9@gmail.com>
2024-06-27 05:35:05 +0100

    rly basic view for testing (todo: fix routes)

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.contrib.auth.models import Group, User
+from rest_framework import permissions, viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(['GET'])
+def test(request):
+    return Response("lolz")
