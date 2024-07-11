4c6588bf74a6484c3bf0efea5fd67cef82f4f27e
silentzer <hamidkorchi9@gmail.com>
2024-07-11 15:10:58 +0100

    refactor router and loginpage

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,60 +1,59 @@
 import { router } from "../routes/routes.js";
-
-// temporary
-function genRandomString(length) {
-	let result = '';
-	let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-	for (let i = 0; i < length; i++) {
-		result += characters.charAt(Math.floor(Math.random() * characters.length));
-	}
-	return result;
-}
-
-function loginUser(event) {
-	event.preventDefault();
-	const username = document.getElementById('email').value;
-	const password = document.getElementById('password').value;
-	if (!username || !password) {
-		return;
-	}
-	const login_data = { username, password };
-	fetch('http://localhost:8000/login', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(login_data)
-    })
-    .then(response => response.json())
-    .then(data => {
-        if ('detail' in data) {
-			const login_err_elem = document.getElementById('login-error-message');
-        	login_err_elem.textContent = data.detail;
-        }
-		else {
-			localStorage.setItem('access_token', data.access);
-			localStorage.setItem('refresh_token', data.refresh);
-            router.navigate("/home");
-        }
-    })
-    .catch((error) => {
-        console.error(`an exception occurred: ${error}`);
-    });
-}
-
-function OAuthLogin(event) {
-	event.preventDefault();
-	let client_id = "u-s4t2ud-9b817b5c046ec73f656311d30af3240cfd77ca2e5a01259a1d39c0223ce5f2fd";
-	let state = genRandomString(16);
-	localStorage.setItem('state', state);
-	console.log(`settings state: `, state);
-	let oauth_url = `https://api.intra.42.fr/oauth/authorize?client_id=${client_id}&redirect_uri=http%3A%2F%2Flocalhost%2F&response_type=code&state=${state}`;
-	window.location.replace(oauth_url);
-}
-
-window.loginUser = loginUser;
-window.OAuthLogin = OAuthLogin;
+import { genRandomString } from "../utils/utils.js";
+import { OAuthIntercept } from "../utils/utils.js";
 
 export class LoginPage extends HTMLElement {
-	connectedCallback() {
+	constructor() {
+		super();
+		this.loginUser = this.loginUser.bind(this);
+		this.OAuthLogin = this.OAuthLogin.bind(this);
+		this.err = "";
+	}
+	async loginUser(event) {
+		event.preventDefault();
+		const username = this.username_elem.value;
+		const password = this.password_elem.value;
+		if (!username || !password) {
+			return;
+		}
+		const login_data = { username, password };
+		try {
+			const response = await fetch('http://localhost:8000/login', {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify(login_data)
+        	});
+			const data = await response.json();
+			if ('detail' in data) {
+				const login_err_elem = document.getElementById('login-error-message');
+				login_err_elem.textContent = data.detail;
+			}
+			else {
+				localStorage.setItem('access_token', data.access);
+				localStorage.setItem('refresh_token', data.refresh);
+				router.navigate("/home");
+			}
+		}
+		catch (error) {
+			console.log(`an exception occured: ${error}`);
+		}
+	}
+	OAuthLogin(event) {
+		event.preventDefault();
+		let state = genRandomString(16);
+		localStorage.setItem('state', state);
+		let oauth_url = `https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-9b817b5c046ec73f656311d30af3240cfd77ca2e5a01259a1d39c0223ce5f2fd&redirect_uri=http%3A%2F%2Flocalhost%2Flogin&response_type=code&state=${state}`;
+		window.location.replace(oauth_url);
+	}
+	async connectedCallback() {
+		if (localStorage.getItem('state')) {
+			console.log(`intercepting state`);
+			this.err = await OAuthIntercept();
+			if (!this.err) {
+				router.navigate("/home");
+				return;
+			}
+		}
 		this.innerHTML = `
 			<div class="login-page">
 				<div class="login-container">
@@ -71,16 +70,23 @@ export class LoginPage extends HTMLElement {
 							<input class="input" id="password" type="password" name="password" placeholder="************">
 						</div>
 						<div class="buttons">
-							<button class="primary-btn" data="Login" onclick="loginUser(event)"></button>
+							<button class="primary-btn" data="Login"></button>
 							<p class="space">OR</p>
-							<button class="secondary-btn" onclick="OAuthLogin(event)"><span><img src="../../assets/images/42.svg" alt="42" class="fortyTwo"></span><span>Login with Intranet</span></button>
+							<button class="secondary-btn"><span><img src="../../assets/images/42.svg" alt="42" class="fortyTwo"></span><span>Login with Intranet</span></button>
 						</div>
 					</form>
 					<p class="ref">New here? create an account by clicking <a class="anchor" href="/register">here</a></p>
 					<p id="login-error-message" class="login-error-message"></p>
 					</div>
-			</div>`;
+			</div>
+		`;
+		this.username_elem = document.getElementById('email');
+		this.password_elem = document.getElementById('password');
+		document.getElementById('login-error-message').textContent = this.err;
+		this.querySelector('.primary-btn').addEventListener('click', this.loginUser);
+		this.querySelector('.secondary-btn').addEventListener('click', this.OAuthLogin);
 	}
+
 };
 
 customElements.define("login-page", LoginPage);
--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,29 +1,12 @@
-import { isAuthenticated } from "../utils/utils.js";
-import { router, public_paths } from "./routes.js";
-import { synchronousFetch } from "../utils/utils.js";
-
-/**
- * when user refreshed the page  it not showing the component,
- * so we need to navigate to the current path
- * also since the home path is /home, and user want to access  home page from root "/" we need to navigate to /home
- */
-if (window.location.pathname === "/")
-  router.navigate("/home");
-else
-  router.navigate(window.location.pathname);
+import { router } from "./routes.js";
 
 // remove the default behavior of the anchor (<a>) tags and added a event to navigate to the clicked link
+// todo: move this somewhere else
 document.body.addEventListener("click", (event) => {
   if (event.target.closest("a")) {
     event.preventDefault();
     const targetHref = event.target.closest("a").getAttribute("href");
-    if (!public_paths.includes(targetHref) && !isAuthenticated()) {
-      router.navigate("/login");
-    } else if (isAuthenticated() && public_paths.includes(targetHref)) {
-      router.navigate("/home");
-    } else {
-      router.navigate(targetHref);
-    }
+    router.navigate(targetHref);
     // check if is the sidebar link and change the active link
     if (event.target.closest(".sidebar_links")) {
       const sidebar_links = document.querySelectorAll(".sidebar_links a");
@@ -37,41 +20,5 @@ document.body.addEventListener("click", (event) => {
   }
 });
 
-// todo: refactor this, and move it somewhere else
-if (localStorage.getItem('state')) {
-  const url = new URL(document.URL);
-  const local_state = localStorage.getItem('state');
-  const ft_state = url.searchParams.get('state');
-  const auth_code = url.searchParams.get('code');
-  localStorage.clear();
-  if (local_state != ft_state) {
-    router.navigate("/login");
-    let err = document.getElementById("login-error-message");
-    err.textContent = "OAuth state mismatch";
-  }
-  else {
-    const oauth_data = { code: auth_code, state: local_state }
-    const headers = { 'Content-Type': 'application/json' }
-    const resp = await synchronousFetch("http://localhost:8000/oauth-login", 'POST', JSON.stringify(oauth_data), headers);
-    const json_resp = await resp.json();
-    if ('error' in json_resp) {
-      router.navigate("/login");
-      let err = document.getElementById("login-error-message");
-      err.textContent = "An internal server error occured";
-    }
-    else {
-      const access_token = json_resp.access;
-      const refresh_token = json_resp.refresh;
-      localStorage.setItem('access_token', access_token);
-      localStorage.setItem('refresh_token', refresh_token);
-      router.navigate("/home");
-    }
-  }
-}
-else if (!isAuthenticated() && !public_paths.includes(window.location.pathname)) {
-  console.log("user is not authenticated, redirecting to /login");  
-  router.navigate("/login");
-}
-else {
-  router.navigate(window.location.pathname);
-}
+console.log(`index.js, url: ${window.location.search} | pathname: ${window.location.pathname}`);
+router.navigate(window.location.pathname);
