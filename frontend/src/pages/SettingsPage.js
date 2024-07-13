ef89751d8a1623c7a4ebed86eceeed8d220ecf18
Mohamed Amine Amazzal <amazzalhusa@gmail.com>
2024-07-13 23:03:40 +0100

    validate user form in settings

--- frontend/src/pages/SettingsPage.js
+++ frontend/src/pages/SettingsPage.js
@@ -3,6 +3,67 @@ import { SettingsUserPFP } from "../components/settings/SettingsUserPFP.js";
 import { ComfirmPasswordPopUp } from "../components/settings/ComfirmPasswordPopUp.js";
 
 export class SettingsPage extends HTMLElement {
+    constructor() {
+        super();
+        this.updateProfile = this.updateProfile.bind(this);
+    }
+    showPopupSetting() {
+        const settings = document.querySelector("settings-page");
+        const confirmPopup = document.createElement("comfirm-password-pop-up");
+        settings.appendChild(confirmPopup);
+        const close_popup = document.getElementById("setting_close_popup");
+        close_popup.onclick = () => {
+            confirmPopup.remove();
+        }
+    }
+
+    changeImageWhenUpload(e) {
+        const file = e.target.files[0];
+        const reader = new FileReader();
+        reader.onload = () => {
+            const img = document.getElementsByClassName("settings_pfp_image")[0];
+            img.src = reader.result;
+        }
+        reader.readAsDataURL(file);
+    }
+
+    getFormData() {
+        const data = {
+            user_name: document.getElementById("user_name").value,
+            user_email: document.getElementById("user_email").value,
+            user_password: document.getElementById("user_password").value,
+        }
+        return data;
+    }
+    async updateProfile(event) {
+        event.preventDefault();
+        const data = this.getFormData();
+        data["confirm_password"] = document.getElementById("settings_password_confirmation").value;
+        if (data["confirm_password"] === "") {
+            document.getElementsByClassName("confirm_password_err")[0].innerHTML = "Password is required, can't be empty";
+            return;
+        }
+        /**
+         * get the return data from @getFormData function
+         * send data to backend here
+        */
+    }
+    async validateForm() {
+        const data = this.getFormData();
+        for (const key in data) {
+            if (data[key])
+                document.getElementsByClassName(key + "_err")[0].innerHTML = "";
+        }
+        let isValid = true;
+        for (const key in data) {
+            if (!data[key]) {
+                document.getElementsByClassName(key + "_err")[0].innerHTML = `${key} is required and can't be empty`;
+                isValid = false;
+            }
+        }
+        return isValid;
+    }
+
   connectedCallback() {
     this.innerHTML = `
             <div class="settings_">
@@ -19,6 +80,17 @@ export class SettingsPage extends HTMLElement {
                 </div>
             </div>
         `;
+        document.getElementById("user_pfp").addEventListener("change", (e) => {
+            this.changeImageWhenUpload(e);
+        });
+        document.getElementById("save_setting_btn").addEventListener("click", () => {
+            this.validateForm().then((isValid) => {
+                if (isValid) {
+                    this.showPopupSetting();
+                    document.getElementById("settings_popup_conf_psw").addEventListener("click", this.updateProfile);
+                }
+            })
+        });
   }
 }
 
