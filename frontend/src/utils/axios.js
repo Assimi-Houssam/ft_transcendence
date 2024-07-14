d4df8a3a24287b15c8f6314686fe4ae9d0aefb78
Mohamed Amine Amazzal <amazzalhusa@gmail.com>
2024-07-14 14:24:24 +0100

    init axios

---
+++ frontend/src/utils/axios.js
@@ -0,0 +1,44 @@
+class Axios {
+    constructor() {
+        this.url = "http://localhost:8000/";
+        this.headers = {};
+    }
+
+    async get(...args) {
+        return await this.request("GET", ...args);
+    }
+
+    async post(...args) {
+        return await this.request("POST", ...args);
+    }
+
+    async put(...args) {
+        return await this.request("PUT", ...args);
+    }
+
+    async delete(...args) {
+        return await this.request("DELETE", ...args);
+    }
+
+    async request(method, endpoint, data = {}, headers = {}) {
+        const response = await fetch(this.url + endpoint, {
+            method,
+            headers: {
+                "Content-Type": "application/json",
+                ...this.headers,
+                ...headers,
+            },
+            body: JSON.stringify(data),
+        });
+        return await response.json();
+    }
+}
+
+export default new Axios();
+
+/**
+ * USAGE:
+ *  import axios from "<path>/utils/axios.js";
+ *  const res =  await Axios.put("/user/update", data, headers);
+ *  NOTE  : headers is optional and can be used to pass additional headers 
+ */
