bd77e1bac6fe8b8878e4c2bca242310e749ec0f9
silentzer <hamidkorchi9@gmail.com>
2024-07-18 07:10:53 +0100

    remove first and last name from /me

--- backend/api/views.py
+++ backend/api/views.py
@@ -16,8 +16,6 @@ import os
 def me(request):
     user = request.user
     return Response({
-        "first_name": user.first_name,
-        "last_name": user.last_name,
         "username": user.username, 
         "email": user.email, 
         "id": user.id, 
