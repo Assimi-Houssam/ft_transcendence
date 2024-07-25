d1d9de2d0e6841a4fe2cae93e80393850d2650c0
silentzer <hamidkorchi9@gmail.com>
2024-07-25 08:00:56 +0100

    setup routing, add test consumer

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -10,11 +10,13 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from api.routing import websocket_urlpatterns
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
+    "websocket": URLRouter(websocket_urlpatterns)
 })
