af72cd7ffea24e81e14a9b1ead95e72442cd407f
silentzer <hamidkorchi9@gmail.com>
2024-07-26 04:47:12 +0100

    consistent backend error messages

--- backend/api/auth.py
+++ backend/api/auth.py
@@ -2,7 +2,7 @@ from django.contrib.auth import authenticate
 from rest_framework_simplejwt.tokens import AccessToken
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.decorators import api_view, permission_classes, authentication_classes
+from rest_framework.decorators import api_view
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from .serializers import UserRegistrationSerializer
 from .models import User
@@ -18,9 +18,9 @@ def register(request):
         return Response({
             'detail': 'User created successfully'
         }, status=status.HTTP_201_CREATED)
-    errs = {'error': []}
+    errs = {'detail': []}
     for err in user.errors:
-        errs['error'].extend(user.errors[err])
+        errs['detail'].extend(user.errors[err])
     return Response(errs, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(["POST"])
@@ -61,7 +61,7 @@ def oauth_login(request):
     r = requests.post(access_token_endpoint, data=params, headers=headers)
     if (r.status_code != requests.codes.ok):
         print("an error has occured")
-        return Response({"error": "An error occured fetching an access token from 42", "detail": r.text})
+        return Response({"detail": "An error occured fetching an access token from 42"}, status=status.HTTP_400_BAD_REQUEST)
     access_token = r.json()["access_token"]
     headers = {
         "Authorization": f"Bearer {access_token}",
@@ -70,7 +70,7 @@ def oauth_login(request):
     r = requests.get(me_endpoint, headers=headers)
     if (r.status_code != requests.codes.ok):
         print(f"an error has occured fetching user info, access_token: {access_token}")
-        return Response({"error": "An error occured fetching user info from 42"})
+        return Response({"detail": "An error occured fetching user info from 42"}, status=status.HTTP_400_BAD_REQUEST)
     user_info = r.json()
     intra_login = user_info['login']
     intra_email = user_info['email']
