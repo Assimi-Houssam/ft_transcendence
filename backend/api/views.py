5d2e43a9d14e38b601ee1e3fc00d2777ead875f8
Mohamed Amine Amazzal <amazzalhusa@gmail.com>
2024-08-12 21:12:59 +0100

    working on search

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,10 @@
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework import status
 from .auth import JWTAuth
+from .serializers import UserSerializer
+from .models import User
 
 @api_view(['GET'])
 @authentication_classes([JWTAuth])
@@ -15,3 +18,18 @@ def me(request):
         "intra_id": user.intra_id, 
         "pfp": user.pfp.url
     })
+
+
+@api_view(['GET'])
+@authentication_classes([JWTAuth])
+@permission_classes([IsAuthenticated])
+def filter_users(request) :
+    query = request.GET["query"]
+    print(query)
+    data = User.objects.filter(username=query).values()
+    print(data);
+    users = UserSerializer(data, many=True)
+    return Response({
+        'detail' : users.data
+    }, status=status.HTTP_200_OK)
+
--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,16 +1,78 @@
+import ApiWrapper from '../utils/ApiWrapper.js'
+import Toast from './Toast.js';
 
 export class Search extends HTMLElement {
     constructor() {
         super();
+        this.delayTime = 250;
+        this.handleDocumentClick = this.handleDocumentClick.bind(this);
     }
+
     onInputFocus(e) {
         const searchResult = document.getElementsByClassName("nav_search_result")[0];
-        searchResult.classList.add("show_search_result");
+        if (searchResult.classList.contains("show_search_result"))
+            return;
+        anime({
+            targets: '.nav_search_result',
+            height: ['0px', '350px'],
+            duration: this.delayTime,
+            easing: 'easeInOutQuad',
+            begin: () => {
+                searchResult.classList.add("show_search_result");
+            }
+        });
+        document.addEventListener('click', this.handleDocumentClick);
     }
-    onInputBlur(e) {
+
+
+    handleDocumentClick(e) {
         const searchResult = document.getElementsByClassName("nav_search_result")[0];
-        searchResult.classList.remove("show_search_result")
+        if (!this.contains(e.target)) {
+            anime({
+                targets: '.nav_search_result',
+                height: ['350px', '0px'],
+                duration: this.delayTime,
+                easing: 'easeInOutQuad',
+                complete: () => {
+                    setTimeout(() => {
+                        searchResult.classList.remove("show_search_result")
+                        document.removeEventListener('click', this.handleDocumentClick);
+                    }, this.delayTime);
+                },
+            });
+        }
     }
+
+    async handleOnChange (e) {
+        const  show_search_result = document.getElementsByClassName("show_search_result")[0];
+        show_search_result.innerHTML = `
+            <div>Searching...</div>
+        `
+        setTimeout( async () => {
+            const res = await ApiWrapper.get(`/users/filter?query=${e.target.value}`);
+            const data = await res.json()
+            console.log(data.detail)
+            if (res.ok) {
+                if (data.detail.length > 0) {
+                    show_search_result.innerHTML =
+                        `
+                            ${data.detail.map((user) => (
+                                `
+                                    <div>
+                                        ${user.username}
+                                    </div
+                                `
+                            ))}
+                        `
+                }else {
+                    show_search_result.innerHTML = `<div>No result found sorry</div>`
+                }
+            }else {
+                show_search_result.innerHTML = `<div>faild to load data</div>`
+            }
+        }, 200);
+    }
+
     connectedCallback() {
         this.innerHTML = `
             <div class="nav_search_ gradient-dark-bg gradient-dark-border">
@@ -22,15 +84,12 @@ export class Search extends HTMLElement {
                 />
             </div>
             <div class="nav_search_result"></div>
-        `
-        // anime({
-        //     targets: '.nav_search_result',
-        //     translateY: [0, 250],
-        //     easing: 'easeOutBounce'
-        // });
+        `;
         const searchBox = document.getElementById("searchBox");
         searchBox.onfocus = (e) => this.onInputFocus(e);
-        searchBox.onblur = (e) => this.onInputBlur(e);
+        searchBox.addEventListener("keyup", (e) => {
+            this.handleOnChange(e)
+        })
     }
 }
 
