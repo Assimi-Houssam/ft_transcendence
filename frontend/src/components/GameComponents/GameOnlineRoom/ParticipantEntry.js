2c944c0d8e8f7cbcce19debfe08db2c7a76ff8d6
silentzer <hamidkorchi9@gmail.com>
2024-08-17 07:41:56 +0100

    refactor TeamSlotBlue/Red, rename to to ParticipantEntry

---
+++ frontend/src/components/GameComponents/GameOnlineRoom/ParticipantEntry.js
@@ -0,0 +1,36 @@
+export class EmptySlot extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <p>Empty Slot</p>
+            <img src="../../../../assets/icons/friends.png">
+        `;
+    }
+}
+
+customElements.define("empty-slot", EmptySlot);
+
+export class ParticipantEntry extends HTMLElement {
+    constructor(username, pfp, banner, teamSide, isHost) {
+        super();
+        this.teamSide = teamSide;
+        this.pfp = pfp;
+        this.username = username;
+        this.banner = banner;
+        this.isHost = isHost;
+    }
+
+    connectedCallback() {
+        this.innerHTML = `
+            <img src="${this.pfp}">
+            <div class="ParticipantUsername">
+                <p>${this.username} ${this.isHost ? `<span style="color: var(--orchid); font-size: 65% ;">(Host)</span>` : ""}</p>
+            </div>`;
+        if (this.banner) {
+            this.style.backgroundImage = `url(${this.banner})`;
+        } else {
+            this.style.backgroundColor = "#212535";
+        }
+    }
+}
+
+customElements.define("pariticipant-entry", ParticipantEntry);
--- frontend/src/components/GameComponents/GameOnlineRoom/RoomCard.js
+++ frontend/src/components/GameComponents/GameOnlineRoom/RoomCard.js
@@ -1,10 +1,14 @@
 import { TeamSlotBlue } from "./TeamSlotBlue.js";
 import { TeamSlotRed } from "./TeamSlotRed.js";
+import { ParticipantEntry, EmptySlot } from "./ParticipantEntry.js";
+
 
 export class RoomCard extends HTMLElement {
   constructor(teamSize=1) {
     super();
     this.teamSize = teamSize;
+    this.blueTeam = {};
+    this.redTeam = {};
   }
 
   connectedCallback() {
@@ -21,7 +25,6 @@ export class RoomCard extends HTMLElement {
                             <p>Red team</p>
                         </div>
                    `) : ""}
-                    <team-slot-red style="width: 85%;"></team-slot-red>
                 </div>
                 ${this.teamSize  === 1 ? (`
                     <div class="vs">
@@ -35,34 +38,34 @@ export class RoomCard extends HTMLElement {
                             <p>Blue team</p>
                         </div>
                     `): ""}
-                    <team-slot-blue style="width: 85%;"></team-slot-blue>
                 </div>
             </div>
         </div>
         `;
-
-        const InviteFriendsBtns = document.getElementsByClassName("InviteFriendsBtn");
-        for (let i = 0; i < InviteFriendsBtns.length; i++) {
-            InviteFriendsBtns[i].addEventListener("click", (e) => {
-                const element = document.querySelector("invite-friends");
-                const usersCardInvite = element.getElementsByClassName("InviteFreindsPopup_content");
-                for (let g = 0; g < usersCardInvite.length; g++) {
-                    usersCardInvite[g].querySelector("button").onclick =  (e) => {
-                        if (InviteFriendsBtns[i]) {
-                            InviteFriendsBtns[i].innerHTML  = `
-                                <div class="TeamSlotBtnContentUser">
-                                    <div class="UserSelected">
-                                        <img src="../../../../assets/images/p1.png">
-                                        <p>rouali <span style="color: var(--orchid); font-size: 65% ;">(Host)</span></p>
-                                    </div>
-                                </div>
-                        `
-                        }
-                    }
-                }
-                element.style.display = "block";
-            })
-        }
+        this.querySelector(".RedTeam").appendChild(new ParticipantEntry("silentzer", "../../../../assets/images/p1.png", "../../../../assets/images/img.png", "blue", true));
+        this.querySelector(".BlueTeam").appendChild(new EmptySlot());
+        // const InviteFriendsBtns = document.getElementsByClassName("InviteFriendsBtn");
+        // for (let i = 0; i < InviteFriendsBtns.length; i++) {
+        //     InviteFriendsBtns[i].addEventListener("click", (e) => {
+        //         const element = document.querySelector("invite-friends");
+        //         const usersCardInvite = element.getElementsByClassName("InviteFreindsPopup_content");
+        //         for (let g = 0; g < usersCardInvite.length; g++) {
+        //             usersCardInvite[g].querySelector("button").onclick =  (e) => {
+        //                 if (InviteFriendsBtns[i]) {
+        //                     InviteFriendsBtns[i].innerHTML  = `
+        //                         <div class="TeamSlotBtnContentUser">
+        //                             <div class="UserSelected">
+        //                                 <img src="../../../../assets/images/p1.png">
+        //                                 <p>rouali <span style="color: var(--orchid); font-size: 65% ;">(Host)</span></p>
+        //                             </div>
+        //                         </div>
+        //                 `
+        //                 }
+        //             }
+        //         }
+        //         element.style.display = "block";
+        //     })
+        // }
   }
 }
 
