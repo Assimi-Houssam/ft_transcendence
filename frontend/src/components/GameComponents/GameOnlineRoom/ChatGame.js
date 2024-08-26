b41bb5aa09881e4ef5191c82c2e03cc859391843
rouali <rouali@student.1337.ma>
2024-08-26 19:32:26 +0100

    chat front

--- frontend/src/components/GameComponents/GameOnlineRoom/ChatGame.js
+++ frontend/src/components/GameComponents/GameOnlineRoom/ChatGame.js
@@ -1,159 +1,73 @@
-export class ChatGame extends HTMLElement {
-    constructor(){
-        super();
-        const now = new Date();
-        const hours = now.getHours();
-        const minutes = now.getMinutes();
-        const time = now.getTime();
-        console.log(now.setHours(0,0,0,0));
-    }
+class SingleMessage extends HTMLElement {
+  constructor(message, username = "rouali") {
+    super();
+    this.message = message;
+    this.classList.add("user-chat");
+    this.username = username;
+    const now = new Date();
+    this.hours = now.getHours();
+    this.minutes = now.getMinutes();
+    this.seconds = now.getSeconds();
+  }
 
-    connectedCallback(){
-        this.innerHTML = `
+  connectedCallback() {
+    this.innerHTML = `
+            <div class ="sidebar-chatbox">
+                <span class="timestamp">${this.hours}:${this.minutes < 10 ? `0${this.minutes}` : `${this.minutes}`}:${this.seconds < 10 ? `0${this.seconds}` : `${this.seconds}`}</span>
+                <span class="username" ${this.username === "rouali" ? `style="color: purple;"` : `style="color: green;"`}>${this.username}</span>
+            </div>
+            <div id="container-msg" class="container-msg">
+                <span class="message">${this.message}</span>
+            </div>
+        `;
+  }
+}
+
+customElements.define("single-message", SingleMessage);
+
+export class ChatGame extends HTMLElement {
+  constructor(username, message) {
+    super();
+    this.username = username;
+    this.message = message;
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
             <div class = "chat-container">
                 <div class="chat-container-content">
                     <div class="chat-header">Room Chat</div>
-
                     <div class="ContainerChat">
                         <div class = "chat-box" id="chat-box">
-                            <div class="chat-box-content">
-                                <div class="user-chat">
-                                    <div class ="sidebar-chatbox">
-                                        <span class="timestamp">12:31:43</span>
-                                        <span class="username" style="color: purple;">mamazza..</span>
-                                    </div>
-                                    <div class="container-msg">
-                                        <span class="message">hi!</span>
-                                    </div>
-                                </div>
-                                <div class="user-chat">
-                                    <div class ="sidebar-chatbox">
-                                        <span class="timestamp">12:31:43</span>
-                                        <span class="username" style="color: purple;">mamazza..</span>
-                                    </div>
-                                    <div class="container-msg">
-                                        <span class="message">hi!</span>
-                                    </div>
-                                </div>
-                                <div class="user-chat">
-                                    <div class ="sidebar-chatbox">
-                                        <span class="timestamp">12:31:43</span>
-                                        <span class="username" style="color: purple;">mamazza..</span>
-                                    </div>
-                                    <div class="container-msg">
-                                        <span class="message">hi!</span>
-                                    </div>
-                                </div>
-                                <div class="user-chat">
-                                    <div class ="sidebar-chatbox">
-                                        <span class="timestamp">12:31:43</span>
-                                        <span class="username" style="color: purple;">mamazza..</span>
-                                    </div>
-                                    <div class="container-msg">
-                                        <span class="message">hi!</span>
-                                    </div>
-                                </div>
-                                <div class="user-chat">
-                                    <div class ="sidebar-chatbox">
-                                        <span class="timestamp">12:31:43</span>
-                                        <span class="username" style="color: purple;">mamazza..</span>
-                                    </div>
-                                    <div class="container-msg">
-                                        <span class="message">hi!</span>
-                                    </div>
-                                </div>
-                                <div class="user-chat">
-                                    <div class ="sidebar-chatbox">
-                                        <span class="timestamp">12:31:43</span>
-                                        <span class="username" style="color: purple;">mamazza..</span>
-                                    </div>
-                                    <div class="container-msg">
-                                        <span class="message">hi!</span>
-                                    </div>
-                                </div>
-                                <div class="user-chat">
-                                    <div class ="sidebar-chatbox">
-                                        <span class="timestamp">12:31:43</span>
-                                        <span class="username" style="color: purple;">mamazza..</span>
-                                    </div>
-                                    <div class="container-msg">
-                                        <span class="message">hi!</span>
-                                    </div>
-                                </div>
-                                <div class="user-chat">
-                                    <div class ="sidebar-chatbox">
-                                        <span class="timestamp">12:31:43</span>
-                                        <span class="username" style="color: purple;">mamazza..</span>
-                                    </div>
-                                    <div class="container-msg">
-                                        <span class="message">hi!</span>
-                                    </div>
-                                </div>
-                                <div class="user-chat">
-                                    <div class ="sidebar-chatbox">
-                                        <span class="timestamp">12:31:43</span>
-                                        <span class="username" style="color: purple;">mamazza..</span>
-                                    </div>
-                                    <div class="container-msg">
-                                        <span class="message">hi!</span>
-                                    </div>
-                                </div>
-                                <div class="user-chat">
-                                    <div class ="sidebar-chatbox">
-                                        <span class="timestamp">12:31:43</span>
-                                        <span class="username" style="color: purple;">mamazza..</span>
-                                    </div>
-                                    <div class="container-msg">
-                                        <span class="message">hi!</span>
-                                    </div>
-                                </div>
-                                <div class="user-chat">
-                                    <div class ="sidebar-chatbox">
-                                        <span class="timestamp">12:31:43</span>
-                                        <span class="username" style="color: purple;">mamazza..</span>
-                                    </div>
-                                    <div class="container-msg">
-                                        <span class="message">hi!</span>
-                                    </div>
-                                </div>
-                                <div class="user-chat">
-                                    <div class ="sidebar-chatbox">
-                                        <span class="timestamp">12:31:43</span>
-                                        <span class="username" style="color: purple;">mamazza..</span>
-                                    </div>
-                                    <div class="container-msg">
-                                        <span class="message">hi!</span>
-                                    </div>
-                                </div>
-                                <div class="user-chat">
-                                    <div class ="sidebar-chatbox">
-                                        <span class="timestamp">12:31:43</span>
-                                        <span class="username" style="color: purple;">mamazza..</span>
-                                    </div>
-                                    <div class="container-msg">
-                                        <span class="message">hi!</span>
-                                    </div>
-                                </div>
-                                <div class="user-chat">
-                                    <div class ="sidebar-chatbox">
-                                        <span class="timestamp">12:31:43</span>
-                                        <span class="username" style="color: purple;">mamazza..</span>
-                                    </div>
-                                    <div class="container-msg">
-                                        <span class="message">hi!</span>
-                                    </div>
-                                </div>
+                            <div id="chats" class="chat-box-content">
                             </div>
                         </div>
                         <div class="input_msg">
                             <input type="text" placeholder="Type something..." class="chat-input" id="chat-input">
-                            <!-- <button id="send-button">Send</button> -->
                         </div>
                     </div>
                 </div>
             </div>
-        `
-    }
+        `;
+
+    const message = document.getElementById("chat-input");
+    const chats = document.getElementById("chats");
+
+    const htmlTagPattern = /<\/?[a-z][\s\S]*>/i;
+
+    this.addEventListener("keypress", (e) => {
+      if (e.key.toLowerCase() == "enter") {
+        if (htmlTagPattern.test(message.value)) {
+          message.value = "";
+        } else {
+          if (String(message.value).trim().length === 0) return;
+          chats.appendChild(new SingleMessage(message.value));
+          message.value = "";
+          chats.scrollTop = chats.scrollHeight;
+        }
+      }
+    });
+  }
 }
 
-customElements.define('chat-game', ChatGame);
+customElements.define("chat-game", ChatGame);
