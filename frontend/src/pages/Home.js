c1437abf1803eeb8a88f186eea2445e69cc8050b
rouali <ridaouali98@gmail.com>
2024-09-01 21:29:25 +0100

    profile

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -5,6 +5,9 @@ export class HomePage extends HTMLElement {
         <div class="home_header_">
           <h1>Home</h1>
         </div>
+        <div class="home_frined_">
+          <p>Welcome to the home page!</p>  
+        </div>
       </div>`
   }
 };
