e52e75712bdb92bed63aa8e43755b82536cb00a8
rouali <ridaouali98@gmail.com>
2024-09-07 05:09:21 +0100

    offline update

--- frontend/src/components/GameComponents/GameOfflineRoom/OfflineGame.js
+++ frontend/src/components/GameComponents/GameOfflineRoom/OfflineGame.js
@@ -23,7 +23,15 @@ export class OfflineGame extends HTMLElement{
         super();
         this.bracketSize = 1;
         this.teamSize = 1;
+        this.gameMode = "pong";
+        this.customElements = [];
         this.bracket = new Tournament(this.teamSize, this.bracketSize);
+        this.gameData = {
+            bracketSize: this.bracketSize,
+            teamSize: this.teamSize,
+            gameMode: this.gameMode,
+            customElements: this.customElements,
+        };
     }
 
     connectedCallback(){
@@ -46,22 +54,36 @@ export class OfflineGame extends HTMLElement{
         this.appendChild(new RoomOptions(true));
         this.appendChild(new TournamentFooter(true));
         this.addEventListener("bracketChange", (evt) => {
-            this.bracketSize = evt.detail;
-            console.log("bracket size:", this.bracketSize, " | teamsize:", this.teamSize);
-            this.bracket.update(this.bracketSize, this.teamSize);
+            this.gameData.bracketSize = evt.detail;
+            console.log("bracket size:", this.gameData.bracketSize, " | teamsize:", this.gameData.teamSize);
+            this.bracket.update(this.gameData.bracketSize, this.gameData.teamSize);
         });
         this.addEventListener("teamSizeChange", (evt) => {
-            this.teamSize = evt.detail;
-            console.log("bracket size:", this.bracketSize, " | teamsize:", this.teamSize);
-            this.bracket.update(this.bracketSize, this.teamSize);
+            this.gameData.teamSize = evt.detail;
+            console.log("bracket size:", this.gameData.bracketSize, " | teamsize:", this.gameData.teamSize);
+            this.bracket.update(this.gameData.bracketSize, this.gameData.teamSize);
         });
-        this.querySelector("#BtnStartGame").addEventListener("click", (e) => {
-            const tournamentinputs = document.querySelectorAll("tournament-group input");
-                const playersName = [];
-                tournamentinputs.forEach(input => {
-                    playersName.push(input.value)
-                })
-                console.log("player Names: ", playersName);
+        this.addEventListener("gameModeChange", (evt) => {
+            this.gameData.gameMode = evt.detail;
+            console.log("game mode:", this.gameData.gameMode);
+            this.bracket.update(this.gameData.gameMode);
+        });
+        this.addEventListener("customizationChange", (evt) => {
+            this.gameData.customElements = evt.detail;
+            console.log("customization :", this.gameData.customElements);
+            this.bracket.update(this.gameData.customElements);
+        });
+        this.querySelector(".BtnStartGame").addEventListener("click", (e) => {
+            const tournamentInputs = document.querySelectorAll("tournament-group input");
+            const playersName = [];
+            tournamentInputs.forEach(input => {
+                playersName.push(input.value);
+            });
+            console.log("Bracket Size: ", this.gameData.bracketSize);
+            console.log("Team Size: ", this.gameData.teamSize);
+            console.log("Game Mode: ", this.gameData.gameMode);
+            console.log("Custom Elements: ", this.gameData.customElements);
+            console.log("Player Names: ", playersName);
         });
 
     }
